a9c5b2e1ae64ba7a46164e5df63f7c5568a5a2ee
America Ocampo <98730589+ame-o@users.noreply.github.com>
2022-05-03 13:44:44 -0700

    added Super class

--- Javascript/ninja.js
+++ Javascript/ninja.js
@@ -1,7 +1,7 @@
 class Ninja {
-    constructor(name, health, speed, strength){
-        this.name;
-        this.health;
+    constructor(name, health){
+        this.name=name;
+        this.health=10;
         this.speed=3;
         this.strength=3;
     }
@@ -21,8 +21,26 @@ class Ninja {
         return this;
     }
 }
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name, 200);
+        this.speed=10;
+        this.strength=10;
+        this.wisdom=10;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log("2+2=4");
+        console.log(`${this.wisdom}`)
+    }
+}
 // example:
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
+
 
